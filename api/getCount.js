9c6592988f8d8c22283f45223c6b2fb179f0870a
juliandiasdacostalima <103572225+juliandiasdacostalima@users.noreply.github.com>
2025-02-22 01:47:12 +0100

    Update getCount.js

--- api/getCount.js
+++ api/getCount.js
@@ -1,6 +1,8 @@
+let count = 0; // Este es un valor fijo por ahora. Se puede cambiar o guardar en otro lugar más adelante.
+
 module.exports = async function (context, req) {
-    const count = 0;  // Este es un valor fijo por ahora. Puedes guardarlo en un lugar como una base de datos.
     context.res = {
-        body: { count }  // Envía el valor del contador como respuesta
+        status: 200,
+        body: { count }  // Envía el valor actual del contador
     };
 };
