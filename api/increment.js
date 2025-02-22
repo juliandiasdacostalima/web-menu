d46e7a535911b786aa44e426e26f1e4796f41d54
juliandiasdacostalima <103572225+juliandiasdacostalima@users.noreply.github.com>
2025-02-22 12:17:08 +0100

    Update increment.js

--- api/increment.js
+++ api/increment.js
@@ -1,20 +1,6 @@
-const { TableClient } = require("@azure/data-tables");
-
-const tableName = "QRCounter";
-const connectionString = process.env.AzureWebJobsStorage;
+let count = 0;
 
 module.exports = async function (context, req) {
-    const tableClient = TableClient.fromConnectionString(connectionString, tableName);
-    const partitionKey = "qr";
-    const rowKey = "counter";
-
-    try {
-        let entity = await tableClient.getEntity(partitionKey, rowKey);
-        entity.count += 1;
-        await tableClient.updateEntity(entity);
-    } catch (error) {
-        await tableClient.createEntity({ partitionKey, rowKey, count: 1 });
-    }
-
+    count++;
     context.res = { status: 200, body: { success: true } };
 };
