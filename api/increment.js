9b488bee1ab40276a3566618e4809bf653161972
juliandiasdacostalima <103572225+juliandiasdacostalima@users.noreply.github.com>
2025-02-22 01:47:26 +0100

    Update increment.js

--- api/increment.js
+++ api/increment.js
@@ -1,7 +1,9 @@
+let count = 0; // Mantén el contador aquí también (si usas un lugar fijo o base de datos, deberás cambiar esto).
+
 module.exports = async function (context, req) {
-    // Aquí puedes incrementar el contador, por ejemplo, guardándolo en un lugar de almacenamiento
+    count++; // Incrementa el valor del contador
     context.res = {
         status: 200,
-        body: { success: true }
+        body: { success: true, count }  // Responde con el nuevo valor del contador
     };
 };
